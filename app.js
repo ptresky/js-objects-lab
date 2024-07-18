3c86b0be02815f0ebb16776bd533c0a49b655816
ptresky <peter_tresky@hotmail.com>
2024-07-18 13:49:08 -0700

    homework complete

--- app.js
+++ app.js
@@ -258,3 +258,12 @@ game.gyms.forEach(gym => {
 
 console.log(game.gyms)
 
+/*
+Exercise 16
+1. Log the entire `game` object to the console. Take a moment to review the changes you've made throughout the exercises.
+
+
+Solve Exercise 16 here:
+*/
+
+console.log(game)
